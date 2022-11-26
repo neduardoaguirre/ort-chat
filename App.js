ba440d6a6c3f90e1dd59bda72df17411d74b9383
Juan Cruz Romero Garcia <rgjuancruz@gmail.com>
2022-11-26 18:46:00 -0300

    Revert "chat 1 vs 1 fail"
    This reverts commit cb0e1618781306154987a3628914bc2daa1b7d66.

--- App.js
+++ App.js
@@ -1,77 +1,35 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { onAuthStateChanged } from 'firebase/auth';
-import React, { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, Button, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { auth } from './config/firebase';
-import { AntDesign } from '@expo/vector-icons';
-import { AuthenticatedUserContext, AuthenticatedUserProvider, getUser, loadUser } from './providers/user.provider';
-import Chat from './screens/Chat';
-import Login from './screens/Login';
-import Profile from './screens/Profile';
-import Signup from './screens/Signup';
-import Home from './screens/Home';
-import Contacts from './screens/Contacts';
-import ChatScreen from './screens/ChatScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { auth } from "./config/firebase";
+import {
+  AuthenticatedUserContext,
+  AuthenticatedUserProvider,
+  loadUser,
+} from "./providers/user.provider";
+import Chat from "./screens/Chat";
+import Login from "./screens/Login";
+import Profile from "./screens/Profile";
+import Signup from "./screens/Signup";
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-      <Button title='Go to Settings' onPress={() => navigation.navigate('Settings')} />
-    </View>
-  );
-}
 
 function ChatStack() {
-  const user = getUser();
   return (
-    <Stack.Navigator defaultScreenOptions={Contacts}>
-      <Stack.Screen name='Chat' component={Chat} />
-      <Stack.Screen name='Profile' component={Profile} />
-      <Stack.Screen name='Contacts' component={Contacts} />
-      <Stack.Screen
-        name='Chats'
-        options={({ route }) => ({
-          title: route.params.name,
-          headerBackTitleVisible: false
-        })}
-      >
-        {(props) => <ChatScreen {...props} user={user} />}
-      </Stack.Screen>
+    <Stack.Navigator defaultScreenOptions={Chat}>
+      <Stack.Screen name="Chat" component={Chat} />
+      <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
-    // <Tab.Navigator
-    //   defaultScreenOptions={Home}
-    //   screenOptions={({ route }) => ({
-    //     tabBarIcon: ({ focused, color, size }) => {
-    //       if (route.name === 'Home') {
-    //         return <AntDesign name='user' size={24} />;
-    //       } else if (route.name === 'Profile') {
-    //         return <AntDesign name='user' size={24} />;
-    //       }
-    //     },
-    //     tabBarInactiveTintColor: 'gray',
-    //     tabBarActiveTintColor: 'tomato'
-    //   })}
-    // >
-    //   <Tab.Screen name='Home' component={HomeScreen} />
-    //   <Tab.Screen name='Chat' component={Chat} />
-    //   <Tab.Screen name='Profile' component={Profile} />
-    //   <Tab.Screen name='Contacts' component={Contacts} />
-    // </Tab.Navigator>
   );
 }
 
 function AuthStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='Login' component={Login} />
-      <Stack.Screen name='Signup' component={Signup} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
   );
 }
@@ -82,13 +40,16 @@ function RootNavigator() {
 
   useEffect(() => {
     // onAuthStateChanged returns an unsubscriber
-    const unsubscribeAuth = onAuthStateChanged(auth, async (authenticatedUser) => {
-      authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+    const unsubscribeAuth = onAuthStateChanged(
+      auth,
+      async (authenticatedUser) => {
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
 
-      await loadUser(authenticatedUser);
+        await loadUser(authenticatedUser);
 
-      setIsLoading(false);
-    });
+        setIsLoading(false);
+      }
+    );
 
     // unsubscribe auth listener on unmount
     return unsubscribeAuth;
@@ -96,14 +57,18 @@ function RootNavigator() {
 
   if (isLoading) {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size='large' />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
       </View>
     );
   }
 
   // If user authenticated go to Chat screen otherwise go to Auth page
-  return <NavigationContainer>{user ? <ChatStack /> : <AuthStack />}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      {user ? <ChatStack /> : <AuthStack />}
+    </NavigationContainer>
+  );
 }
 
 export default function App() {

0718d08e62ecf2415a2b6e40415942ff3144c000
Juan Cruz Romero Garcia <rgjuancruz@gmail.com>
2022-11-29 19:20:15 -0300

    Add chatlist empty placeholder

--- src/screens/chat/ChatList.js
+++ src/screens/chat/ChatList.js
@@ -1,6 +1,9 @@
+import { Ionicons } from '@expo/vector-icons';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import {
   Badge,
   Box,
+  Button,
   Divider,
   Flex,
   Heading,
@@ -11,44 +14,8 @@ import {
 } from 'native-base';
 import React, { useContext, useLayoutEffect, useMemo, useState } from 'react';
 import { ScrollView } from 'react-native';
-import { Button } from 'native-base';
-import { Ionicons } from '@expo/vector-icons';
-import { AuthenticatedUserContext } from '../../context/userContext';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import { database } from '../../config/firebase';
-
-const mockChats = [
-  {
-    room: {
-      id: 'xxx',
-      name: 'Anime'
-    },
-    chat: {
-      id: 'xxx',
-      lastMessage: new Date()
-    }
-  },
-  {
-    room: {
-      id: 'xxx',
-      name: 'Economia'
-    },
-    chat: {
-      id: 'xxx',
-      lastMessage: new Date()
-    }
-  },
-  {
-    room: {
-      id: 'xxx',
-      name: 'Tecnologia'
-    },
-    chat: {
-      id: 'xxx',
-      lastMessage: new Date()
-    }
-  }
-];
+import { AuthenticatedUserContext } from '../../context/userContext';
 
 export const ChatListComponent = ({ navigation }) => {
   const [chats, setChats] = useState([]);
@@ -89,67 +56,77 @@ export const ChatListComponent = ({ navigation }) => {
             Chats
           </Heading>
           <Divider />
-          {React.Children.toArray(
-            chatBelongs.map((chat, i) => {
-              return (
-                <Box
-                  alignSelf="center"
-                  rounded="8"
-                  overflow="hidden"
-                  borderWidth="1"
-                  borderColor="coolGray.300"
-                  width="90%"
-                  shadow="3"
-                  bg="coolGray.100"
-                  p="5"
-                  marginTop={3}
-                >
-                  <HStack alignItems="center">
-                    <Badge
-                      colorScheme="darkBlue"
-                      _text={{
-                        color: 'white'
-                      }}
-                      variant="solid"
-                      rounded="4"
-                    >
-                      {chat.name}
-                    </Badge>
-                    <Spacer></Spacer>
-                    {/* <Flex marginTop={3}>
-                      <Text
-                        fontSize={10}
-                        color="coolGray.800"
-                        fontWeight="bold"
-                      >
-                        Ultima actvidad
-                      </Text>
-                      <Text fontSize={10} color="coolGray.800">
-                        {/* {chat.chat.lastMessage.toDateString()} */}
-                    {/* </Text> */}
-                    {/* </Flex> */}
-                  </HStack>
-                  <Text
-                    color="coolGray.800"
-                    mt="3"
-                    fontWeight="medium"
-                    fontSize="xl"
+          {chatBelongs?.length ? (
+            React.Children.toArray(
+              chatBelongs.map((chat, i) => {
+                return (
+                  <Box
+                    alignSelf="center"
+                    rounded="8"
+                    overflow="hidden"
+                    borderWidth="1"
+                    borderColor="coolGray.300"
+                    width="90%"
+                    shadow="3"
+                    bg="coolGray.100"
+                    p="5"
+                    marginTop={3}
                   >
-                    {chat.info}
-                  </Text>
-                  <Flex direction="row" marginTop={3} alignItems="flex-end">
-                    <Button width="20" fontSize={12} onPress={() => goTo(chat)}>
-                      Chatear
-                    </Button>
-                    <Spacer></Spacer>
-                    <Text color="coolGray.800" fontWeight="bold">
-                      <Ionicons name="people-outline" size={24} color="black" />{' '}
-                      {chat.users.length}
+                    <HStack alignItems="center">
+                      <Badge
+                        colorScheme="darkBlue"
+                        _text={{
+                          color: 'white'
+                        }}
+                        variant="solid"
+                        rounded="4"
+                      >
+                        {chat.name}
+                      </Badge>
+                      <Spacer></Spacer>
+                    </HStack>
+                    <Text
+                      color="coolGray.800"
+                      mt="3"
+                      fontWeight="medium"
+                      fontSize="xl"
+                    >
+                      {chat.info}
                     </Text>
-                  </Flex>
-                </Box>
-              );
-            })
+                    <Flex direction="row" marginTop={3} alignItems="flex-end">
+                      <Button
+                        width="20"
+                        fontSize={12}
+                        onPress={() => goTo(chat)}
+                      >
+                        Chatear
+                      </Button>
+                      <Spacer></Spacer>
+                      <Text color="coolGray.800" fontWeight="bold">
+                        <Ionicons
+                          name="people-outline"
+                          size={24}
+                          color="black"
+                        />{' '}
+                        {chat.users.length}
+                      </Text>
+                    </Flex>
+                  </Box>
+                );
+              })
+            )
+          ) : (
+            <HStack
+              margin={5}
+              space={2}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Text color="coolGray.800" fontSize="md">
+                Aun no te has subscrito a ninguna Sala, subscribete para
+                comenzar a chatear!
+              </Text>
+            </HStack>
           )}
         </VStack>
       </ScrollView>
